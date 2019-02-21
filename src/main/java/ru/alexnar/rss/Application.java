89887d0bd3cb8f358f3c789452fa6657640f20e6
Alexander Naryjny <alexigrnar@gmail.com>
2019-02-21 20:17:51 +0300

    entry point

---
+++ src/main/java/ru/alexnar/rss/Application.java
@@ -0,0 +1,12 @@
+package ru.alexnar.rss;
+
+public class Application {
+  private void run() {
+
+  }
+
+  public static void main(String[] args) {
+    Application application = new Application();
+    application.run();
+  }
+}
