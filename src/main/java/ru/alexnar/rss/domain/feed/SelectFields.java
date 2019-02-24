9e123e1d4617a86670ed9ce856b4c0265c90c0f7
alexnar <alexigrnar@gmail.com>
2019-02-24 13:47:23 +0300

    select fields

---
+++ src/main/java/ru/alexnar/rss/domain/feed/SelectFields.java
@@ -0,0 +1,21 @@
+package ru.alexnar.rss.domain.feed;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SelectFields {
+  private static final String FIELD_ELEMENT_IDENTIFIER = "element_";
+  public final List<String> generalFields;
+  public final List<String> elementFields;
+
+  public SelectFields(List<String> args) {
+    this.generalFields = args.stream()
+            .filter(arg -> !arg.startsWith(FIELD_ELEMENT_IDENTIFIER))
+            .collect(Collectors.toList());
+    this.elementFields = args.stream()
+            .filter(arg -> arg.startsWith(FIELD_ELEMENT_IDENTIFIER))
+            .map(arg -> arg.substring(FIELD_ELEMENT_IDENTIFIER.length()))
+            .collect(Collectors.toList());
+
+  }
+}
---
+++ src/test/java/ru/alexnar/rss/domain/feed/SelectFieldsTest.java
@@ -0,0 +1,35 @@
+package ru.alexnar.rss.domain.feed;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class SelectFieldsTest {
+  @Test
+  public void correctFieldsParsed() {
+    List<String> fields = Arrays.asList("author", "language", "element_title", "element_link", "element_description");
+    SelectFields selectFields = new SelectFields(fields);
+    List<String> generalFields = selectFields.generalFields;
+    List<String> elementFields = selectFields.elementFields;
+    assertEquals(2, generalFields.size());
+    assertEquals(3, elementFields.size());
+    assertTrue(generalFields.containsAll(Arrays.asList("author", "language")));
+    assertTrue(elementFields.containsAll(Arrays.asList("title", "link", "description")));
+  }
+
+  @Test
+  public void emptyListReturnEmptyLists() {
+    SelectFields selectFields = new SelectFields(new ArrayList<>());
+    List<String> generalFields = selectFields.generalFields;
+    List<String> elementFields = selectFields.elementFields;
+    assertNotNull(generalFields);
+    assertNotNull(elementFields);
+    assertTrue(generalFields.isEmpty());
+    assertTrue(elementFields.isEmpty());
+  }
+
+}
