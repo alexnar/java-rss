5e21726cc9e32c7d73f48e06c253609142309191
alexnar <alexigrnar@gmail.com>
2019-02-27 11:40:08 +0300

    restructure packages, add modal abstraction

---
+++ src/main/java/ru/alexnar/rss/model/feed/FeedProperties.java
@@ -0,0 +1,42 @@
+package ru.alexnar.rss.model.feed;
+
+import ru.alexnar.rss.model.feed.select.FeedFields;
+import ru.alexnar.rss.model.feed.select.SelectFields;
+
+import java.util.concurrent.TimeUnit;
+
+public class FeedProperties {
+  private static final int DEFAULT_ELEMENTS_COUNT = Integer.MAX_VALUE;
+  private static final Period DEFAULT_PERIOD = new Period(20, TimeUnit.SECONDS);
+
+  public final String url;
+  public int elementCount;
+  public Period period;
+  public SelectFields selectFields;
+  public String outputFileName;
+
+
+  public FeedProperties(String url) {
+    this(url, DEFAULT_ELEMENTS_COUNT, DEFAULT_PERIOD, allFields());
+  }
+
+  public FeedProperties(String url, int elementCount, Period period, SelectFields selectFields) {
+    this.url = url;
+    this.elementCount = elementCount;
+    this.period = period;
+    this.outputFileName = fileNameFromUrl(url);
+    this.selectFields = selectFields;
+  }
+
+  public void setOutputFileName(String outputFileName) {
+    this.outputFileName = outputFileName;
+  }
+
+  private String fileNameFromUrl(String url) {
+    return url.replaceAll("/", "_");
+  }
+
+  private static SelectFields allFields() {
+    return new SelectFields(FeedFields.fields());
+  }
+}
